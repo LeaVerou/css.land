0eeb064af1521e7ddefe123142b1fad31f6fde16
Lea Verou <lea@verou.me>
2022-02-15 16:12:49 -0500

    Add box-model app

---
+++ box-model/index.js
@@ -0,0 +1,38 @@
+function textContent(node) {
+	if (node?.[Mavo.toNode]) {
+		return textContent(node[Mavo.toNode].element);
+	}
+	else if (node instanceof Node) {
+		let ret = "";
+
+		if (node.nodeType === 1) {
+			if (node.matches("input, textarea")) {
+				ret += "checked" in node? "" : node.value;
+			}
+			else if (node.matches("select")) {
+				ret += [...node.selectedOptions].map(o => o.textContent).join("\n");
+			}
+			else {
+				let cs = getComputedStyle(node);
+
+				if (cs.display === "none" || cs.visibility === "hidden" || cs.getPropertyValue("--custom-visibility")?.trim() === "hidden") {
+					ret += "";
+				}
+				else {
+					let before = getComputedStyle(node, "::before");
+					let after = getComputedStyle(node, "::after");
+					// FIXME this will break with nontrivial generated content
+					before = before.content === "none" ? "" : before.content.match(/^['"](.*)['"]$/)?.[1];
+					after = after.content === "none" ? "" : after.content.match(/^['"](.*)['"]$/)?.[1];
+
+					ret += before + [...node.childNodes].map(textContent).join("") + after;
+				}
+			}
+		}
+		else if (node.nodeType === 3) {
+			ret += node.textContent;
+		}
+
+		return ret;
+	}
+}
